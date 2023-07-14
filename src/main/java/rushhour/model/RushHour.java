e881508b249b714ae028b1a767ce637f87ebb7de
Lily Chau <lc5682@rit.edu>
2023-07-14 14:15:38 -0500

    rush_hour

--- src/main/java/rushhour/model/RushHour.java
+++ src/main/java/rushhour/model/RushHour.java
@@ -1,5 +1,18 @@
 package rushhour.model;
 
+import java.io.IOException;
+
 public class RushHour {
-    private static final int BOARD_DIM = 6;
+    public static final int BOARD_DIM = 6;
+    public static final char RED_SYMBOL = 'R';
+    public static final char EMPTY_SYMBOL = '-';
+    public static final Position EXIT_POS = new Position (2, 5);
+
+    private final String filename;
+
+    public RushHour (String filename) throws IOException {
+        this.filename = filename;
+    }
 }
+
+   
