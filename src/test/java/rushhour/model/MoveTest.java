2a4e9b4c5a082b1616dc0130ca345d370be92841
Gavin McConnell <gavinwmcconnell@gmail.com>
2023-07-20 15:35:25 -0400

    all testing done for now

--- src/test/java/rushhour/model/MoveTest.java
+++ src/test/java/rushhour/model/MoveTest.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test;
 public class MoveTest {
     @Test
     public void move1(){
-        Move m = new Move('a', Direction.UP);
+        Move m = new Move('A', Direction.UP);
+        char a1 = m.getSymbol();
+        Direction a2 = m.getDirect();
+        String a3 = m.toString();
+        assertEquals('A', a1);
+        assertEquals(Direction.UP, a2);
+        assertEquals("A: UP", a3);
+
     }
 }
