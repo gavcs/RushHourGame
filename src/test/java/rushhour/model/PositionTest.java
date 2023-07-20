4cd202e34df5fde8138a23cac6a5eebed336ff3b
Gavin McConnell <gavinwmcconnell@gmail.com>
2023-07-20 15:33:07 -0400

    positiontest done

--- src/test/java/rushhour/model/PositionTest.java
+++ src/test/java/rushhour/model/PositionTest.java
@@ -4,5 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class PositionTest {
-    
+    @Test
+    public void positionTest(){
+        Position p1 = new Position(0,0);
+        int a1 = p1.getRow();
+        int a2 = p1.getCol();
+        boolean a3 = p1.equals(new Position(0,0));
+        assertEquals(a1, 0);
+        assertEquals(a2, 0);
+        assertEquals(true, a3);
+    }
 }
