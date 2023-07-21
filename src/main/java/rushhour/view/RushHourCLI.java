2b0e956bbeb9029462a3dab06ab3ebb7c075c18b
Lily Chau <lc5682@rit.edu>
2023-07-21 00:20:50 -0500

    list of commands

--- src/main/java/rushhour/view/RushHourCLI.java
+++ src/main/java/rushhour/view/RushHourCLI.java
@@ -10,18 +10,27 @@ import rushhour.model.RushHour;
 public class RushHourCLI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Filename: ");
+        System.out.print("Enter a Filename: ");
         String filename = scanner.nextLine();
         RushHour rushHour;
         try{
             rushHour = new RushHour(filename);
             System.out.println(rushHour);
+            boolean forceQuit =  false;
+                System.out.println (" === List of Commands ===\n" +
+                    "'help' | display a list of commands\n" +
+                    "'symbol + direction' | moves the vehicle(symbol) one space in the given direction\n" +
+                    "'hint' | display an available move\n" +
+                    "'reset' | resets the current filename board\n" +
+                    "'quit' | quits the game\n" +
+                    "-------------------------------------");
+               
         }catch(IOException e){
-        System.out.println("IOException occured, check filename.");
+            
+        System.out.println("IOException, check filename format (ex: 03_00.csv).");
 
     }
     scanner.close(); 
-
     }
 
 }
