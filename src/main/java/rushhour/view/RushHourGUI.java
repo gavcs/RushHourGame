80c7a8c3b507512d2be16a0ed8589bdbb96c8bab
Lily Chau <lc5682@rit.edu>
2023-07-26 10:12:02 -0500

    rushhour gui - needs work

--- src/main/java/rushhour/view/RushHourGUI.java
+++ src/main/java/rushhour/view/RushHourGUI.java
@@ -4,4 +4,10 @@ import javafx.application.Application;
 
 public class RushHourGUI extends Application {
     
+    private Map<Character, Node> vehiclesOnBoard;
+    private Node hintLabel;
+    private Button hintButton;
+    private List<Node> infoLabels;
+    private RushHour rh;
+    private GridPane gpBoard;
 }
